85e62ea018958c458b2f8ad4e6a003feef2809ec
Samet Gokkaya <sametgokkayax@gmail.com>
2024-02-17 01:31:08 +0300

    added mock backend json-server

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from '../'
+
+function Spinner() {
+   return <img src={spinner} alt="loading.." style={{
+      width: '50px',
+      margin: 'auto',
+      display: 'block'
+   }} />
+}
+
+export default Spinner
